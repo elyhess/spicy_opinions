8815e8bca8a8486458c14063d1aeb4f189321b76
Ely Hess <ely.hess@me.com>
2021-05-15 10:24:38 -0600

    add modules

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import "dotenv/config"
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
+import morgan from "morgan"
+import helmet from "helmet";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+
 
 app.use("/api/v1/users", routes.user);
 
