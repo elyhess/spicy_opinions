ba3ccefb343c2874ff0d83ee238f46a6389b59e9
Ely Hess <ely.hess@me.com>
2021-05-17 16:42:51 -0600

    Add article controller w/ crud

---
+++ controllers/article.controller.js
@@ -0,0 +1,73 @@
+const Article = require("../models").Article;
+const Comment = require("../models").Comment;
+
+exports.getArticle = async (req, res) => {
+  const { id } = req.params;
+  const article = await Article.findOne({where: {id}, include: {model: Comment}
+  })
+
+  if (!article) {
+    return res.status(404).send({message: `Article with id ${id} not found.`})
+  }
+
+  return res.send(article)
+}
+
+exports.getArticles = async (req, res) => {
+  const articles = await Article.findAll({order: ["id"]});
+  if (!articles.length) {
+    return res.status(400).send({message: "No articles found."});
+  }
+
+  return res.send(articles)
+}
+
+exports.createArticle = async (req, res) => {
+  const { userId, title, body } = req.body;
+
+  try {
+    let newArticle = await Article.create({userId, title, body});
+    return res.send(newArticle)
+  } catch (error) {
+    return res.status(400).send({message: `Error ${error.message}`})
+  }
+}
+
+exports.updateArticle = async (req, res) => {
+  const { id } = req.params;
+  const { body, title } = req.body;
+  const article = await Article.findOne({where: {id}})
+
+  if (!article) {
+    return res.status(400).send({message: `Article with id ${id} not found.`})
+  }
+
+  if (!body || !title) {
+    return res.status(400).send({message: "Title and body must be provided"})
+  }
+
+  try {
+    article.title = title
+    article.body = body
+    article.save();
+    return res.status(201).send({message: "Article update successfully."})
+  } catch (error) {
+    return res.status(400).send({message: `Error ${error.message}`})
+  }
+}
+
+exports.deleteArticle = async (req, res) => {
+  const { id } = req.params;
+  const article = await Article.findOne({where: { id }})
+
+  if (!article) {
+    return res.status(400).send({message: `Article with id ${id} not found.`})
+  }
+
+  try {
+    await article.destroy();
+    return res.send({message: "Article has been deleted"});
+  } catch(err) {
+    return res.status(500).send({message: `Error ${err.message}`});
+  }
+}
