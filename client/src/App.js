c65f3d0087b471509aea65eee0f19ce9cb05fe94
Ely Hess <ely.hess@me.com>
2021-05-19 08:34:58 -0600

    Remove un-used useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {Fragment, useState} from "react";
+import {Fragment} from "react";
 import NavComponent from "./components/Nav";
 import MainContent from "./components/MainContent"
 
 function App() {
-  const [ body, setBody] = useState("");
-  const [ title, setTitle] = useState("");
-
   return (
       <Fragment>
         <NavComponent />
