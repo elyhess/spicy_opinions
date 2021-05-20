038d3662a83c63fffb9f79622cf93f5faae92e4f
Ely Hess <ely.hess@me.com>
2021-05-20 16:01:02 -0600

    Remove Link from react-router-dom

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-function Nav() {
+function Nav(props) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -32,49 +32,66 @@ function Nav() {
                   </div>
                 </div>
               </div>
-              <div className="-mr-2 flex md:hidden">
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    type="button"
-                    className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    aria-controls="mobile-menu"
-                    aria-expanded="false"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  {!isOpen ? (
-                      <svg
-                          className="block h-6 w-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                      >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
-                      </svg>
-                  ) : (
-                      <svg
-                          className="block h-6 w-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                      >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                  )}
-                </button>
+              <div className="-mr-2 flex">
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    <div className="inactive-link">
+                      <NavLink to="/login" activeClassName="active-link" onClick={props.openModal}>Log In</NavLink>
+                    </div>
+                    {/*<div className="inactive-link">*/}
+                    {/*  <NavLink to="/logout" activeClassName="active-link" onClick={props.openModal}>Log Out</NavLink>*/}
+                    {/*</div>*/}
+                    {/*<div className="inactive-link">*/}
+                    {/*  <NavLink to="/register" activeClassName="active-link" onClick={props.openModal}>Register</NavLink>*/}
+                    {/*</div>*/}
+                  </div>
+                </div>
+
+                <div className="md:hidden">
+                  <button
+                      onClick={() => setIsOpen(!isOpen)}
+                      type="button"
+                      className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      aria-controls="mobile-menu"
+                      aria-expanded="false"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    {!isOpen ? (
+                        <svg
+                            className="block h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                        >
+                          <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M4 6h16M4 12h16M4 18h16"
+                          />
+                        </svg>
+                    ) : (
+                        <svg
+                            className="block h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                        >
+                          <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                    )}
+                  </button>
+
+                </div>
               </div>
             </div>
           </div>
