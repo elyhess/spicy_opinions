d1aaed4f41d5b3e4e0d95608c393f5acf8720754
Ely Hess <ely.hess@me.com>
2021-05-20 11:33:47 -0600

    Add new article form tailwind styling

--- client/src/components/InputHotTake.js
+++ client/src/components/InputHotTake.js
@@ -30,27 +30,34 @@ function InputHotTake(props) {
     <div className="w-full max-w-screen border border-gray-600 rounded-2xl py-3 px-5 mb-5 shadow-lg">
       <div className="flex flex-wrap space-x-1">
         <div className="text-2xl font-bold">
-          What's on your mind???
+          What's on your mind?
         </div>
       </div>
-
-      <form className="d-flex" onSubmit={onSubmitForm}>
-        <input type="text"
-               placeholder="title"
-               className="input"
-               value={titleInput}
-               onChange={e => setTitleInput(e.target.value)}
-        />
-        <input type="textarea"
-               placeholder="body"
-               className="input"
-               value={bodyInput}
-               onChange={e => setBodyInput(e.target.value)}
-        />
-        <button className="btn btn-primary">
-          Post!
-        </button>
-      </form>
+      <div className="pt-4">
+        <form onSubmit={onSubmitForm}>
+          <input type="text"
+                 placeholder="Your Spicy Opinion Here."
+                 className="w-full h-12 px-4 mb-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                 value={titleInput}
+                 onChange={e => setTitleInput(e.target.value)}
+          />
+          <div className="flex justify-between">
+          <textarea
+              placeholder="Explain yourself!"
+              className="w-11/12 h-20 px-3 py-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+              value={bodyInput}
+              onChange={e => setBodyInput(e.target.value)}
+          />
+            <button className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-red-500 hover:bg-red-600 hover:shadow-lg">
+              <img
+                  className="h-8 w-7 mr-1"
+                  src="pepper.png"
+                  alt="Workflow"
+              />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
 
   </Fragment>
