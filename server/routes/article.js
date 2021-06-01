52df0e8ccbc05117065cddf977fbc3208fee19ad
Ely Hess <ely.hess@me.com>
2021-06-01 12:30:30 -0600

    Add passport auth protected route

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 const article = require("../controllers/article.controller")
 const router = Router();
+import passport from "passport";
+import "../auth/passport";
 
-router.get("/", article.getArticles)
+router.get("/", passport.authenticate("jwt", { session: false }),article.getArticles)
 router.get("/:id", article.getArticle)
 router.post("/new", article.createArticle)
 router.patch("/:id", article.updateArticle)
