98de7e06d43650c0bd43a8f0239840d749e9a5a7
Ely Hess <ely.hess@me.com>
2021-06-01 12:30:38 -0600

    Move confetti config

---
+++ client/src/config/ConfettiConfig.js
@@ -0,0 +1,15 @@
+const config = {
+  angle: "274",
+  spread: "360",
+  startVelocity: 40,
+  elementCount: 70,
+  dragFriction: 0.12,
+  duration: "6900",
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "623px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
+export default config;
