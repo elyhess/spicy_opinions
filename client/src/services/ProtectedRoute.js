e95b6337599de9e2eb03ecb235e19638cc2ec308
Ely Hess <ely.hess@me.com>
2021-06-02 13:09:01 -0600

    Update protected route

--- client/src/services/ProtectedRoute.js
+++ client/src/services/ProtectedRoute.js
@@ -1,32 +1,20 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import AuthService from "../services/AuthService";
+import {Redirect, Route} from "react-router-dom";
 
-const ProtectedRoute = ({
-                                 component: Component,
-                                 ...rest
-                               }) => {
+function ProtectedRoute({component: Component, user: User, ...rest}) {
   return (
       <Route
           {...rest}
-          render={props => {
-            if (AuthService.getCurrentUser()) {
-              return <Component {...props} />;
+          render={(props) => {
+            if (User) {
+              return <Component {...rest} {...props} />;
             } else {
-              return (
-                  <Redirect
-                      to={{
-                        pathname: "/",
-                        state: {
-                          from: props.location
-                        }
-                      }}
-                  />
-              );
+              return <Redirect to={{pathname: "/login", state: {from: props.location}}}/>;
             }
           }}
       />
-  );
-};
+  )
+}
 
 export default ProtectedRoute;
+
