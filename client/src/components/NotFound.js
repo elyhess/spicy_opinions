35db955661f8e48511a9dfd401b809dd79aaf810
Ely Hess <ely.hess@me.com>
2021-06-03 20:34:45 -0600

    Add 404 component

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../notFound.scss';
+
+const NotFound = () => {
+  return (
+      <div className='container pt-12'>
+        <div className="gandalf">
+          <div className="fireball"></div>
+          <div className="skirt"></div>
+          <div className="sleeves"></div>
+          <div className="shoulders">
+            <div className="hand left"></div>
+            <div className="hand right"></div>
+          </div>
+          <div className="head">
+            <div className="hair"></div>
+            <div className="beard"></div>
+          </div>
+        </div>
+        <div className="mt-12 text-2xl text-center">
+          <p>404 - You Shall Not Pass</p>
+          <p>Uh oh, Gandalf is blocking the way!<br />Maybe you have a typo in the url? Or you meant to go to a different location?</p>
+        </div>
+        <p className="pt-12 text-center text-2xl text-red-500 hover:text-black rounded-md text-base font-semibold"><Link to='/'>Back to Home</Link></p>
+      </div>
+  )
+}
+
+export default NotFound;
