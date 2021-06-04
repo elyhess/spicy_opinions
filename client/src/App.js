91e5e1ed7f187c863a377735460df2b10c5837c5
Ely Hess <ely.hess@me.com>
2021-06-03 20:36:49 -0600

    Refactor InputHotTakes & HotTakesList component into SpiciesPage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,33 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import NavComponent from "./components/Nav";
 import Login from "./components/Login";
 import AuthService from "./services/AuthService";
 import ProtectedRoute from "./services/ProtectedRoute";
 import ProfilePage from "./components/ProfilePage";
-import InputHotTake from "./components/InputHotTake";
-import HotTakeList from "./components/HotTakeList";
-import authHeader from "./services/DataService";
+import SpiciesPage from "./components/SpiciesPage";
+import NotFoundPage from "./components/NotFound";
+import Logout from "./components/Logout"
 
 function App() {
   const user = AuthService.getCurrentUser()
-  const [ hotTakes, setHotTakes ] = useState([]);
-
-  async function getHotTakes() {
-    if (user) {
-      const response = await fetch("http://localhost:4000/api/v1/articles", {
-        method: "GET",
-        headers: authHeader()
-      })
-      const hotTakesArray = await response.json();
-      setHotTakes(hotTakesArray)
-    }
-  }
-
-  useEffect(() => {
-  }, [hotTakes]); // any time hottakes is changed re-render
-
-  useEffect(() => {
-    if (user) {
-      getHotTakes();
-    }
-  }, []); // on page load
 
   return (
       <>
-        <NavComponent openModal={Login}/>
-        <Route exact path="/about" />
-        <Route exact path="/" />
-        <Route exact path="/login" component={Login} user={user}/>
-        <ProtectedRoute exact path="/profile" user={user} component={ProfilePage} />
-        <ProtectedRoute exact path="/spicies" user={user} component={InputHotTake} getHotTakes={getHotTakes} />
-        <ProtectedRoute exact path="/spicies" user={user} component={HotTakeList} hotTakes={hotTakes} />
+        <NavComponent />
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path="/about" />
+          <Route exact path="/login" render={()=>(
+              user ? (alert("You are already logged in!"), (<Redirect to="/profile"/>)) : (<Login />)
+          )} />
+          <Route exact path="/logout" render={()=>(
+              !user ? (alert("You can't sign out if you aren't logged in."), (<Redirect to="/about"/>)) : (<Logout />)
+          )} />
+          <ProtectedRoute exact path="/profile" user={user} component={ProfilePage} />
+          <ProtectedRoute exact path="/spicies" user={user} component={SpiciesPage} />
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
       </>
 
   );
