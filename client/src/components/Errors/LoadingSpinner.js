ddf260a91107ac4fea261f83779a1ce346c4b60c
Ely Hess <ely.hess@me.com>
2021-06-12 18:29:23 -0600

    Add loading spinner component

---
+++ client/src/components/Errors/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner(props) {
+  return (
+      <div className="text-center pt-20 text-2xl">
+        Loading....
+      </div>
+  );
+}
+
+export default LoadingSpinner;
