5d19a3dc0b00683551953f69b9c86129f462824e
Ely Hess <ely.hess@me.com>
2021-06-12 18:29:03 -0600

    Change async getHotTake to sync

--- client/src/components/HotTakeComments/HotTake.js
+++ client/src/components/HotTakeComments/HotTake.js
@@ -1,17 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import HotTakeCommentList from "./HotTakeCommentList"
 import InputComment from "./InputComment"
+import NotFound from "../Errors/NotFound"
+import LoadingSpinner from "../Errors/LoadingSpinner";
 import { useParams } from "react-router-dom";
+
 function HotTake(props) {
   const [ hotTake, setHotTake ] = useState(null)
+  const [ loading, setLoading ] = useState(true)
   const { id } = useParams();
 
-  async function getHotTake() {
-    const response = await fetch(`http://localhost:4000/api/v1/articles/${id}`, {
+  function getHotTake() {
+    fetch(`http://localhost:4000/api/v1/articles/${id}`, {
       method: "GET"
+    }).then (data => data.json())
+      .then (data => {
+        if (data.id) {
+          setHotTake(data)
+        }
+        setLoading(false);
+    }).catch((error) => {
+      console.error(error)
     })
-    const hotTake = await response.json();
-    setHotTake(hotTake)
   }
 
   useEffect(() => {
@@ -21,8 +31,26 @@ function HotTake(props) {
 
   return (
       <>
-        <HotTakeCommentList hotTake={hotTake}/>
-        <InputComment />
+      {loading ? (
+          <LoadingSpinner/>
+      ) : (
+        <>
+          {hotTake ?
+              (
+                <>
+                  <HotTakeCommentList hotTake={hotTake}/>
+                  <InputComment />
+                </>
+              )
+              :
+              (
+                <>
+                  <NotFound/>
+                </>
+              )
+          }
+        </>
+      ) }
       </>
   );
 }
